082a4e3341d8a2228478b175235d26636088c8d6
Maksim <167882024+panaromixxx@users.noreply.github.com>
2024-10-20 15:01:18 +0700

    first commit

---
+++ core/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+
+class User(AbstractUser):
+    avatars = models.ImageField(upload_to='images/avatars/', blank=True)
